7cdc25f7b5603e0c6bdf47712c0cdb3f7e791100
ErikZakaryan <erik.zakaryan@alu.bue.edu.ar>
2019-06-22 19:50:58 -0300

    Creando componentes y modificando funciones JS para que se cargue cada componente en el index

--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
+window.onload = function () {
     $('header').load('./templates/menu.html');
     $('footer').load('./templates/footer.html');
-});
+    $('main').load('./templates/inicio.html');
+    var cambioComponente = function(nombrePagina){
+        console.log("hola")
+        nombrePagina = $("#nav-link").val()
+        $("main").innerHTML()
+        $("main").load('./templates/' + nombrePagina)
+    }
+};
